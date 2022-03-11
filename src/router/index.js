85526f26e80354a5af8be5c1ca4796409004e720
linhtunguyen <linhcva31399@gmail.com>
2022-03-11 17:16:17 +0700

    detail page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import DetailView from '../views/DetailView.vue'
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    component: DetailView
   },
   {
     path: '/about',
